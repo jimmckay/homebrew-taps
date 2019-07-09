133cd0da38f3365044571f68e15ab64650cd8bec
Jim McKay <jimmckay@users.noreply.github.com>
2019-07-08 18:02:45 -0700

    Update notarizer.rb
    v1.0.0.1

--- Formula/notarizer.rb
+++ Formula/notarizer.rb
@@ -1,8 +1,8 @@
 class Notarizer < Formula
   desc "Notarize and Staple App or DMG"
   homepage "https://github.com/jimmckay/notarizer/"
-  url "https://github.com/jimmckay/notarizer/archive/v1.0.0.0.tar.gz"
-  sha256 "39afc63b8b7c71962a11990888d96144a99dcdc9fd6153f86fb224cf2aca7561"
+  url "https://github.com/jimmckay/notarizer/archive/v1.0.0.1.tar.gz"
+  sha256 "2da2aab9616097d112eab0da9a5a6a5ba5d429c492d4963ab63a57864ca53f7f"
 
   def install
     bin.install "notarizer"
